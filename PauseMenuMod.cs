a8a96aab8b03e3ec4dd54ae541247ab571c84f6a
SmidgeonE <finlaynugent@gmail.com>
2021-04-13 23:27:07 +0100

    Added Basic Layout + Time.timescale
    TO DO: Ensure all animations etc are paused.

--- PauseMenuMod.cs
+++ PauseMenuMod.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using BepInEx;
+using UnityEngine;
+using HarmonyLib;
+using UnityEngine.EventSystems;
 
 namespace PauseMenu
 {
-    [BepInPlugin("MP5_Bolt_Release", "MP5_Bolt_Release", "1.1.0")]
-    public class PauseMenuMod
+    [BepInPlugin("PauseMenuMod", "PauseMenuMod", "1.0.0")]
+    public class PauseMenuMod : BaseUnityPlugin
     {
+        private static bool _gameIsPaused;
         
+        private void Update()
+        {
+            CheckForButtonPress();
+        }
+
+        private void CheckForButtonPress()
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Debug.Log("Player pressing escape");
+                if(_gameIsPaused) TurnOnGame();
+                else TurnOffGame();
+            }
+        }
+
+        private void TurnOffGame()
+        {
+            Debug.Log("Pausing");
+            _gameIsPaused = !_gameIsPaused;
+
+            Time.timeScale = 0f;
+        }
+
+        private void TurnOnGame()
+        {
+            Debug.Log("Entering Game again");
+            _gameIsPaused = !_gameIsPaused;
+
+            Time.timeScale = 1f;
+        }
     }
 }
