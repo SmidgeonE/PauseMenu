72aef35561763c8ffc72b79d27e7f00a50747b3e
SmidgeonE <finlaynugent@gmail.com>
2021-04-13 23:13:50 +0100

    Initial commit

---
+++ PauseMenuMod.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BepInEx;
+
+namespace PauseMenu
+{
+    [BepInPlugin("MP5_Bolt_Release", "MP5_Bolt_Release", "1.1.0")]
+    public class PauseMenuMod
+    {
+        
+    }
+}
